1274ff68050439ee699d1cc69925cad0cb7623d9
Urvish Patel <115104788+urvishpatel2507@users.noreply.github.com>
2024-09-18 09:32:56 +0000

    let var const and console.table done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
 console.log("jay ganesh and jay dada")
+
+const studentNumber = 15
+let name="urvish"
+var surName="patel"
+fatherName="piyushbhai"
+let studentState;
+
+// studentNumber=20
+console.table([studentNumber,name,surName,fatherName,studentState])
